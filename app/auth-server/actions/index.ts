c819149cfc00ff788740446b9dbf2ebc25b5fa80
FYRAS Mostaycer <firasmoustaycer@takiacademyteam.com>
2024-07-22 18:57:24 +0100

    google oauth

--- app/auth-server/actions/index.ts
+++ app/auth-server/actions/index.ts
@@ -3,34 +3,34 @@
 import { createSupabaseServerClient } from "@/lib/supabase/server";
 
 export async function signUpWithEmailAndPassword(data: {
-	email: string;
-	password: string;
-	confirm: string;
+  email: string;
+  password: string;
+  confirm: string;
 }) {
-	const supabase = await createSupabaseServerClient();
-	const result = await supabase.auth.signUp({
-		email: data.email,
-		password: data.password,
-	});
-	return JSON.stringify(result);
+  const supabase = await createSupabaseServerClient();
+  const result = await supabase.auth.signUp({
+    email: data.email,
+    password: data.password,
+  });
+  return JSON.stringify(result);
 }
 
 export async function signInWithEmailAndPassword(data: {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }) {
-	const supabase = await createSupabaseServerClient();
-	const result = await supabase.auth.signInWithPassword(data);
-	return JSON.stringify(result);
+  const supabase = await createSupabaseServerClient();
+  const result = await supabase.auth.signInWithPassword(data);
+  return JSON.stringify(result);
 }
 
-export async function loginWithGithub() {
-	const supabase = await createSupabaseServerClient();
+export async function loginWithGoogle() {
+  const supabase = await createSupabaseServerClient();
 
-	supabase.auth.signInWithOAuth({
-		provider: "github",
-		options: {
-			redirectTo: `http://localhost:3000/auth-server/callback`,
-		},
-	});
+  supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: `http://localhost:3000/auth-server/callback`,
+    },
+  });
 }
--- app/oauth/components/AuthForm.tsx
+++ app/oauth/components/AuthForm.tsx
@@ -4,23 +4,23 @@ import createSupabaseBrowerClient from "@/lib/supabase/client";
 import React from "react";
 
 export default function AuthForm() {
-	const supabase = createSupabaseBrowerClient();
+  const supabase = createSupabaseBrowerClient();
 
-	const loginWithGithub = async () => {
-		await supabase.auth.signInWithOAuth({
-			provider: "github",
-			options: {
-				redirectTo: `${location.origin}/oauth/callback`,
-			},
-		});
-	};
+  const loginWithGoogle = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${location.origin}/oauth/callback`,
+      },
+    });
+  };
 
-	return (
-		<div className="space-y-5">
-			<h1>Login with oAuth</h1>
-			<Button className="w-full" onClick={loginWithGithub}>
-				Login with Github
-			</Button>
-		</div>
-	);
+  return (
+    <div className="space-y-5">
+      <h1>Login with oAuth</h1>
+      <Button className="w-full" onClick={loginWithGoogle}>
+        Login with Google
+      </Button>
+    </div>
+  );
 }

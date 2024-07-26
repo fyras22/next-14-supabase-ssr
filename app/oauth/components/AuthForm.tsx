35c497547e74cfe03959798cc7a99adab1f5ed93
FYRAS Mostaycer <firasmoustaycer@takiacademyteam.com>
2024-07-26 15:58:09 +0100

    redirect to todo

--- app/oauth/components/AuthForm.tsx
+++ app/oauth/components/AuthForm.tsx
@@ -10,7 +10,7 @@ export default function AuthForm() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${location.origin}/todo`,
+        redirectTo: `${location.origin}/oauth/callback`,
       },
     });
   };
